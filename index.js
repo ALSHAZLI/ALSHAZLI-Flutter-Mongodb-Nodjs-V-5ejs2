6528566b8f2c8b82d7f3e8039c72b2bd55bb0210
apolgasim <vandeek911.com>
2021-09-08 20:58:40 +0300

    firest comit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 8080 || process.env.PORT
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb://vana:6df1oMi0BMK8zpZh@cluster0-shard-00-00.iczji.mongodb.net:27017,cluster0-shard-00-01.iczji.mongodb.net:27017,cluster0-shard-00-02.iczji.mongodb.net:27017/dbUser2?ssl=true&replicaSet=atlas-pcxqif-shard-0&authSource=admin&retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=>console.log('database conected'))
+    .catch(err =>{console.log(err)});
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use('/',require('./routes/user.route'))
+app.listen(port,()=>{
+    console.log('port running on '+port)
+})
---
+++ routes/user.route.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const User = require('../models/user.model')
+const router = express.Router()
+
+
+router.post('/Register',(req,res)=>{
+    User.findOne({email:req.body.email},(err,user)=>{
+        if(err){
+            console.log(err)
+            res.json(err)
+        }else{
+            if(user==null){
+                const user = User({
+                    email:req.body.email,
+                    password:req.body.password
+                })
+                user.save()
+                .then((err)=>{
+                    if(err){
+                        console.log(err)
+                        res.json(err)
+                    }else{
+                        console.log(user)
+                        res.json(user)
+                    }
+                    
+                })
+            }else{
+
+            res.json({
+                message:'email is not avilable'
+            })   
+            }
+        }
+    })
+    
+})
+router.get('/Register',(req,res)=>{
+    res.send('login page sasasasasasa');
+})
+
+router.post('/Login',async (req,res,err)=>{
+    var username = req.body.username,
+      password = req.body.password;
+
+      try {
+        var user = await User.findOne({ username: username }).exec();
+        var password = await User.findOne({ password: password }).exec();
+        try{
+            if(!user || !password) {
+                res.status(403).send({success: false, msg: 'Authentication Failed, User not found'})
+                res.redirect("/Register");
+                console.log('not valid user');
+                
+                
+            
+            }else{
+                console.log('sucsess user and sucsess password')
+                res.json(user)
+               
+                    
+            }
+        }
+        catch
+            {(error) =>{ 
+                res.status(500).json({
+                  error: error
+                });
+            console.log('blabala loser')
+        }}
+        // var password = await User.findOne({ password: password }).exec();
+        // if(!password) {
+            
+        //     console.log('not valid User');
+        // }
+        // else{
+        //     console.log('sucsess password')
+        // }
+        
+        
+    } catch (error) {
+      console.log(error)
+      
+    }
+})
+module.exports = router
